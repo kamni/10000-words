a903aa043b352956064a34bdbce05b53ba69b6af
J Leadbetter <j@jleadbetter.com>
2025-02-21 15:44:47 +0100

    practice01 prototype

---
+++ prototypes/practice01/models.py
@@ -0,0 +1,79 @@
+"""
+Copyright (C) J Leadbetter <j@jleadbetter.com>
+Affero GPL v3
+"""
+
+from enum import StrEnum
+from typing import Any, List, Optional
+
+from pydantic import BaseModel
+
+from common.utils.languages import LanguageCode
+
+
+class Document(BaseModel):
+    display_name: Optional[str] = None
+    author: Optional[str] = None
+    language_code: Optional[LanguageCode] = 'en'
+    sentences: Optional[List['Sentence']] = []
+
+    class Config:
+        use_enum_values = True
+
+
+class Sentence(BaseModel):
+    text: str
+    language_code: Optional[LanguageCode] = 'en'
+    ordering: Optional[int]
+    enabled_for_study: Optional[bool] = False
+    display_text: Optional[List['DisplayText']] = []
+    translations: Optional[List['Sentence']] = []
+
+    class Config:
+        use_enum_values = True
+
+
+class DisplayText(BaseModel):
+    sentence_ordering: int
+    ordering: int
+    text: str
+    language_code: Optional[LanguageCode] = 'en'
+    word: 'Word'
+
+    class Config:
+        use_enum_values = True
+
+
+class WordStatus(StrEnum):
+    """
+    Statuses for a certain word or word phrase.
+
+    How the statuses work:
+
+    1. Not Set: User has not yet set a status on this word
+    2. Ignored: this word doesn't have much value in learning
+       -- e.g. articles like "the", conjunctions like "and",
+       and prepositions that don't translate well.
+
+    3. Learned: words that the user feels fluently comfortable with.
+
+    4. To Learn: adds the words to a queue to introduce in future lessons.
+
+    5. Learning: currently in circulation for learning exercises.
+    """
+    not_set = 'Not Set'
+    ignored = 'Ignored'
+    learned = 'Learned'
+    to_learn = 'To Learn'
+    learning = 'Learning'
+
+
+class Word(BaseModel):
+    sentence_ordering: int
+    display_text_ordering: int
+    text: str
+    language_code: Optional[LanguageCode] = 'en'
+    status: Optional[WordStatus] = 'Not Set'
+
+    class Config:
+        use_enum_values = True
--- prototypes/practice01/scripts.py
+++ prototypes/practice01/scripts.py
@@ -8,86 +8,20 @@ Affero GPL v3
 import os
 import sys
 from pathlib import Path
-from typing import Any, Dict, List, Optional
+from typing import Dict, List, Optional
 
 import toml
-from pydantic import BaseModel, ValidationError
+from pydantic import ValidationError
 
 PROJECT_DIR = Path(__file__).resolve().parent.parent.parent
 if PROJECT_DIR.as_posix() not in sys.path:
     sys.path.append(PROJECT_DIR.as_posix())
 
-from common.utils.languages import LanguageCode, language_name_to_code
+from common.utils.languages import language_name_to_code
+from prototypes.practice01.models import Document, Sentence
 
 
 DATA_DIR = Path(__file__).resolve().parent / 'data'
-
-
-class Document(BaseModel):
-    display_name: Optional[str] = None
-    author: Optional[str] = None
-    language_code: Optional[LanguageCode] = 'en'
-    sentences: Optional[List['Sentence']] = []
-
-    class Config:
-        use_enum_values = True
-
-
-class Sentence(BaseModel):
-    text: str
-    language_code: Optional[LanguageCode] = 'en'
-    ordering: Optional[int]
-    enabled_for_study: Optional[bool] = False
-    display_text: Optional[List['DisplayText']] = []
-
-    class Config:
-        use_enum_values = True
-
-
-class DisplayText(BaseModel):
-    sentence_ordering: int
-    ordering: int
-    text: str
-    language_code: Optional[LanguageCode] = 'en'
-    word: 'Word'
-
-    class Config:
-        use_enum_values = True
-
-
-class WordStatus(StrEnum):
-    """
-    Statuses for a certain word or word phrase.
-
-    How the statuses work:
-
-    1. Not Set: User has not yet set a status on this word
-    2. Ignored: this word doesn't have much value in learning
-       -- e.g. articles like "the", conjunctions like "and",
-       and prepositions that don't translate well.
-
-    3. Learned: words that the user feels fluently comfortable with.
-
-    4. To Learn: adds the words to a queue to introduce in future lessons.
-
-    5. Learning: currently in circulation for learning exercises.
-    """
-    not_set = 'Not Set'
-    ignored = 'Ignored'
-    learned = 'Learned'
-    to_learn = 'To Learn'
-    learning = 'Learning'
-
-
-class Word(BaseModel):
-    sentence_ordering: int
-    display_text_ordering: int
-    text: str
-    language_code: Optional[LanguageCode] = 'en'
-    status: Optional[WordStatus] = 'Not Set'
-
-    class Config:
-        use_enum_values = True
 
 
 class TOMLCreator:
